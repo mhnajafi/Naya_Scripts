b57b057be121364159da1f129b0542baa625a19b
mhnajafi <mhnch75@gmail.com>
2024-10-28 16:53:22 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,31 +2,29 @@
 
 ver=1.23
 
-
-
-# Define red color code
-RED='\033[0;31m'
-NC='\033[0m' 
-GREEN='\033[0;32m'
-
-
-
 imgtool=/home/mhn/zephyrproject/bootloader/mcuboot/scripts/imgtool.py
 key=/home/mhn/zephyrproject/bootloader/mcuboot/root-rsa-2048.pem
 encrypt=/home/mhn/zephyrproject/bootloader/mcuboot/enc-rsa2048-priv.pem
 
-
 infile=/home/mhn/zephyrproject/applications/blinky/build/zephyr/zephyr.bin
 outfile=/home/mhn/zephyrproject/applications/blinky/build/zephyr/zephyr.signed.confirmed.encrypted.bin
 
-uf2=/home/mhn/zephyrproject/applications/uf2script/uf2conv.py
-uf2file=/home/mhn/zephyrproject/applications/uf2script/flash.uf2
-uf2dist=/media/mhn/XIAO-SENSE/flash.uf2
 uf2dir=/media/mhn/XIAO-SENSE
 
 slot_size=0x54000
 Slot1_address=0x8c000
 
+
+
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+uf2=$SCRIPT_DIR/uf2conv.py
+uf2file=$SCRIPT_DIR/flash.uf2
+uf2dist=$uf2dir/flash.uf2
+# Define red color code
+RED='\033[0;31m'
+NC='\033[0m' 
+GREEN='\033[0;32m'
 
 if [ -f "$infile" ]; then
 	$imgtool sign --key $key -E $encrypt --header-size 0x200 -S $slot_size --version $ver --confirm --pad  $infile $outfile
@@ -44,5 +42,4 @@ else
 	echo -e "${RED}============( Error )=============${NC}"
 	echo -e "${RED}UF2 Directory does not exist.${NC}"
 fi
-
 
